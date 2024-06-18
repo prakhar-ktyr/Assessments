b59c5001a9c32a8d579d807439ab06d382705047
prakhar-ktyr <katiyar.4@iitj.ac.in>
2024-06-18 09:28:42 +0530

    add-course component added

--- src/app/components/add-course/add-course.component.ts
+++ src/app/components/add-course/add-course.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CourseService } from '../../services/course.service';
+import { Course } from '../../models/course';
 
 @Component({
   selector: 'app-add-course',
   templateUrl: './add-course.component.html',
-  styleUrl: './add-course.component.scss'
+  styleUrls: ['./add-course.component.scss'],
 })
-export class AddCourseComponent {
+export class AddCourseComponent implements OnInit {
 
+  courseAddForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private courseService: CourseService) {
+    this.courseAddForm = this.fb.group({
+      courseName: ['', Validators.required],
+      courseDescription: ['', Validators.required],
+      categoryId: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  isInvalid(controlName: string): boolean {
+    const control = this.courseAddForm.get(controlName);
+    return control?.invalid && (control?.touched || !control?.pristine) || false;
+  }
+
+  onSubmit(frmValue: any): void {
+    console.log('Form Value:', frmValue);
+
+    const newCourse: Course = {
+      id: 0, // Set an appropriate id or generate it as needed
+      courseName: frmValue.courseName,
+      courseDescription: frmValue.courseDescription,
+      categoryId: frmValue.categoryId
+    };
+
+    this.courseService.addCourse(newCourse).subscribe(
+      (response: Course[]) => {
+        console.log('Course added successfully', response);
+      },
+      (error: any) => {
+        console.error('Error adding course', error);
+      }
+    );
+  }
 }
