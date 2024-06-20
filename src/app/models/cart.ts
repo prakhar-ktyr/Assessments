5e0ee665d14025b3d0736a151f2998971e6023fd
suvankit99 <suvankit2001@gmail.com>
2024-06-20 15:18:48 +0530

    Changed cart service

--- src/app/models/cart.ts
+++ src/app/models/cart.ts
@@ -2,9 +2,15 @@ import { Assessment } from "./assessment";
 
 export class Cart{
     id:number ; 
-    arrAssessments:Assessment[] ; 
-    constructor(id:number , arrAssessments:Assessment[]){
+    arrAssessments:Assessment[] ;
+    userId:number ; 
+    quantity:number[] ;
+    total:number ; 
+    constructor(id:number , userId:number,  arrAssessments:Assessment[] , quantity:number[] , total:number){
         this.id = id ; 
+        this.userId = userId ; 
         this.arrAssessments = arrAssessments ; 
+        this.quantity = quantity ; 
+        this.total = total ; 
     }
 }
