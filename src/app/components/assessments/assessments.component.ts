0fe0904dbdd3243ccc301f57cf6b20448ea79b4e
prakhar-ktyr <katiyar.4@iitj.ac.in>
2024-06-21 10:35:11 +0530

    Assessments page updated with pagination

--- src/app/components/assessments/assessments.component.ts
+++ src/app/components/assessments/assessments.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Assessment } from '../../models/assessment';
 import { AssessmentService } from '../../services/assessment.service';
 import { Router } from '@angular/router';
@@ -6,20 +6,44 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-assessments',
   templateUrl: './assessments.component.html',
-  styleUrl: './assessments.component.scss'
+  styleUrls: ['./assessments.component.scss']
 })
-export class AssessmentsComponent {
+export class AssessmentsComponent implements OnInit {
 
-  arrAssessments:Assessment[] = []
-  assessment:Assessment = new Assessment(0, "", "", "", [], 0)
+  arrAssessments: Assessment[] = [];
+  pagedAssessments: Assessment[] = [];
+  assessment: Assessment = new Assessment(0, "", "", "", [], 0);
+  currentPage: number = 1;
+  pageSize: number = 9;
+  totalPages: number = 0;
+  totalPagesArray: number[] = [];
 
-  constructor(private assessmentService: AssessmentService , private router : Router) { 
+  constructor(private assessmentService: AssessmentService, private router: Router) { }
+
+  ngOnInit(): void {
     this.assessmentService.getAssessments().subscribe((assessments: Assessment[]) => {
       this.arrAssessments = assessments;
+      this.totalPages = Math.ceil(this.arrAssessments.length / this.pageSize);
+      this.totalPagesArray = Array(this.totalPages).fill(0).map((x, i) => i + 1);
+      this.updatePagedAssessments();
     });
   }
-  
-  displayDetails(aid:number,  aName: string, aDescription: string) {
-    this.router.navigate(["viewDetails/" + aid]) ; 
+
+  updatePagedAssessments(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedAssessments = this.arrAssessments.slice(startIndex, endIndex);
+  }
+
+  changePage(page: number): void {
+    if (page < 1 || page > this.totalPages) {
+      return;
+    }
+    this.currentPage = page;
+    this.updatePagedAssessments();
+  }
+
+  displayDetails(aid: number, aName: string, aDescription: string): void {
+    this.router.navigate(["viewDetails/" + aid]);
   }
 }
