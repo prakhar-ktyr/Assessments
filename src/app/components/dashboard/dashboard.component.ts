0b99ccca79f502c4256334cf461fc250a6e44f80
suvankit99 <suvankit2001@gmail.com>
2024-06-24 14:53:56 +0530

    changed dashboard logic

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Assessment } from '../../models/assessment';
 import { LocalStorageService } from '../../services/local-storage.service';
+import { AssessmentService } from '../../services/assessment.service';
+import { TraineeService } from '../../services/trainee.service';
+import { AssessmentTrainees } from '../../models/assessmentTrainess';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,7 +14,8 @@ export class DashboardComponent {
   arrAssessments : Assessment[] = [] ; 
   loggedUserId : string = "" ; 
   loggedUserRole: string = "" ; 
-  constructor(private localStorageService : LocalStorageService){
+  arrAssessmentTrainees : AssessmentTrainees[] = [] ; 
+  constructor(private localStorageService : LocalStorageService , private assessmentService : AssessmentService , private traineeService : TraineeService){
     let id = this.localStorageService.getItem("loggedUserId") ; 
     this.loggedUserId = id === null ? "0" : id ; 
 
@@ -19,12 +23,48 @@ export class DashboardComponent {
     this.loggedUserRole = role === null ? "trainee" : role ; 
 
     if(role === 'Admin'){
-
+     // just return arrAsessments 
+     this.assessmentService.getAssessments().subscribe(data => {
+      this.arrAssessments = data ; 
+    })
     }
-    else if(role === 'trainee'){
-      
+    else if(role === 'Trainee'){
+      this.traineeService.getAssessmentTrainess().subscribe(data => {
+        this.arrAssessmentTrainees = data ; 
+        console.log(this.arrAssessmentTrainees) ; 
+
+        let arrId:string[] = [] ; 
+        for(let i = 0; i < this.arrAssessmentTrainees.length ; i++){
+          if(this.arrAssessmentTrainees[i].traineeId === this.loggedUserId){
+            arrId.push(this.arrAssessmentTrainees[i].assessmentId) ;
+          }
+        }
+
+        let arrAssess:Assessment[] = [] ; 
+        this.assessmentService.getAssessments().subscribe(data => {
+          arrAssess = data ; 
+        })
+
+        for(let i = 0 ; i < arrId.length ; i++){
+          for(let j = 0 ; j < arrAssess.length ; j++){
+            if(String(arrAssess[j].id) === arrId[i]){
+              this.arrAssessments.push(arrAssess[j]);
+            }
+          }
+        }
+
+        console.log(this.arrAssessments) ;
+      })
     }
     else{
+      this.assessmentService.getAssessments().subscribe(data => {
+        for(let i = 0 ; i < data.length ; i++){
+          // console.log(data[i].facultyId) ; 
+          if(String(data[i].facultyId) === this.loggedUserId){
+            this.arrAssessments.push(data[i]);
+          }
+        }
+      })
 
     }
   }
--- src/app/services/trainee.service.ts
+++ src/app/services/trainee.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Trainee } from '../models/trainee';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { AssessmentTrainees } from '../models/assessmentTrainess';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +10,13 @@ import { Trainee } from '../models/trainee';
 export class TraineeService {
 
   arrTrainees : Trainee[] = [] ; 
-  constructor() { 
+  baseUrl: string = 'http://localhost:3000';
+  httpHeader = {
+    headers: new HttpHeaders({
+      'content-type': 'application/json',
+    }),
+  };
+  constructor(private httpClient: HttpClient) { 
     
   }
 
@@ -17,5 +26,18 @@ export class TraineeService {
 
   getTraineeByID(id:number){
     return this.arrTrainees[id] ; 
+  }
+  getAssessmentTrainess(){
+    return this.httpClient.get<AssessmentTrainees[]>(this.baseUrl + "/assessmentTrainees", this.httpHeader).pipe(catchError(this.httpError));
+  }
+  private httpError(error: HttpErrorResponse) {
+    let msg = '';
+    if (error.error instanceof ErrorEvent) {
+      msg = error.error.message;
+    } else {
+      msg = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(msg);
+    return throwError(msg);
   }
 }
