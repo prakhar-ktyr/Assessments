eff0099d6c3011d002eef43f3af91665965975e0
suvankit99 <suvankit2001@gmail.com>
2024-06-24 14:10:50 +0530

    changed data.json for assessmentTrainees

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Assessment } from '../../models/assessment';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  arrAssessments : Assessment[] = [] ; 
+  loggedUserId : string = "" ; 
+  loggedUserRole: string = "" ; 
+  constructor(private localStorageService : LocalStorageService){
+    let id = this.localStorageService.getItem("loggedUserId") ; 
+    this.loggedUserId = id === null ? "0" : id ; 
 
+    let role = this.localStorageService.getItem("role") ; 
+    this.loggedUserRole = role === null ? "trainee" : role ; 
+
+    if(role === 'Admin'){
+
+    }
+    else if(role === 'trainee'){
+      
+    }
+    else{
+
+    }
+  }
 }
