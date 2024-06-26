893e7ce6144ff0a698a54d0d455c5b3f01764777
suvankit99 <suvankit2001@gmail.com>
2024-06-26 14:43:51 +0530

    take assessment works

--- src/app/components/attempt-assessment/attempt-assessment.component.ts
+++ src/app/components/attempt-assessment/attempt-assessment.component.ts
@@ -12,49 +12,50 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AttemptAssessmentComponent implements OnInit {
   assessmentId : number = 0 ; 
   arrQuestions : Question[] = [] ; 
-
-  // arrQuestions: Question[] = [
-  //   {
-  //     id: 1,
-  //     text: 'What is the capital of France?',
-  //     type: 'multiple-choice',
-  //     choices: ['Paris', 'London', 'Berlin', 'Madrid'],
-  //     correctAnswer: 'Paris'
-  //   },
-  //   {
-  //     id: 2,
-  //     text: 'The Earth is flat.',
-  //     type: 'true-false',
-  //     correctAnswer: false
-  //   }
-  // ];
-
-  questionForm: FormArray ;
-
+  questionForm!: FormArray ;
+  finalScore:number = -1; 
   constructor(private fb:FormBuilder , private router : Router, private activatedRoute: ActivatedRoute , private assessmentService:AssessmentService){
     this.activatedRoute.params.subscribe(params => {
       this.assessmentId = params['id'] ;
     })
     this.assessmentService.getAssessmentById(this.assessmentId).subscribe(data => {
-      this.arrQuestions = data.questions ; 
+      this.arrQuestions = data.questions ;
+      // console.log(data.questions) ; 
       console.log(this.arrQuestions) ; 
+      this.questionForm = this.fb.array(this.arrQuestions.map(() => this.fb.group({
+        answer: ['', Validators.required]
+      })));
     })
-    this.questionForm = this.fb.array(this.arrQuestions.map(() => this.fb.group({
-      answer: ['', Validators.required]
-    })));
   }
   ngOnInit(): void {
-    this.questionForm = this.fb.array(this.arrQuestions.map(() => this.fb.group({
-      answer: ['', Validators.required]
-    })));
+ 
   }
 
   submitAnswers(): void {
     const answers = this.questionForm.value;
     console.log('Submitted answers:', answers);
+    this.finalScore = this.getScore(answers) ; 
     // Handle the submission logic here
   }
 
+  getScore(answers:any){
+    console.log(answers) ; 
+    let score : number = 0 ;  
+    for(let i = 0; i < this.arrQuestions.length ; i++){
+      let ans = answers[i].answer ; 
+      if(this.arrQuestions[i].type === 'true-false'){
+        if(this.arrQuestions[i].correctAnswer === ans.toString()){
+          score += 1 ; 
+        }
+      }
+      else{
+        if(this.arrQuestions[i].correctAnswer === ans){
+          score += 1 ; 
+        }
+      }
+    }
+    return score ; 
+  }
   getFormGroup(index: number): FormGroup {
     return this.questionForm.at(index) as FormGroup;
   }
