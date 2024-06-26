9242be40585aaf234700c495d657ab86e0d544cd
suvankit99 <suvankit2001@gmail.com>
2024-06-26 15:34:50 +0530

    changed attendance service

--- src/app/components/attempt-assessment/attempt-assessment.component.ts
+++ src/app/components/attempt-assessment/attempt-assessment.component.ts
@@ -3,67 +3,91 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Question } from '../../models/questions';
 import { AssessmentService } from '../../services/assessment.service';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Attendance } from '../../models/attendance';
+import { AttendanceService } from '../../services/attendance.service';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-attempt-assessment',
   templateUrl: './attempt-assessment.component.html',
-  styleUrl: './attempt-assessment.component.scss'
+  styleUrl: './attempt-assessment.component.scss',
 })
 export class AttemptAssessmentComponent implements OnInit {
-  hasStarted:boolean= false ;
-  assessmentId : number = 0 ; 
-  arrQuestions : Question[] = [] ; 
-  questionForm!: FormArray ;
-  finalScore:number = -1; 
-  startTime : Date = new Date(); 
-  constructor(private fb:FormBuilder , private router : Router, private activatedRoute: ActivatedRoute , private assessmentService:AssessmentService){
-    this.activatedRoute.params.subscribe(params => {
-      this.assessmentId = params['id'] ;
-    })
-    this.assessmentService.getAssessmentById(this.assessmentId).subscribe(data => {
-      this.arrQuestions = data.questions ;
-      // console.log(data.questions) ; 
-      console.log(this.arrQuestions) ; 
-      this.questionForm = this.fb.array(this.arrQuestions.map(() => this.fb.group({
-        answer: ['', Validators.required]
-      })));
-    })
+  hasStarted: boolean = false;
+  assessmentId: number = 0;
+  arrQuestions: Question[] = [];
+  questionForm!: FormArray;
+  finalScore: number = -1;
+  startTime: Date = new Date();
+  loggedUserId: string = '';
+  constructor(
+    private localStorageService: LocalStorageService,
+    private fb: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private assessmentService: AssessmentService,
+    private attendanceService: AttendanceService
+  ) {
+    this.loggedUserId = this.localStorageService.getItem('loggedUserId') || '0';
+
+    this.activatedRoute.params.subscribe((params) => {
+      this.assessmentId = params['id'];
+    });
+    this.assessmentService
+      .getAssessmentById(this.assessmentId)
+      .subscribe((data) => {
+        this.arrQuestions = data.questions;
+        // console.log(data.questions) ;
+        console.log(this.arrQuestions);
+        this.questionForm = this.fb.array(
+          this.arrQuestions.map(() =>
+            this.fb.group({
+              answer: ['', Validators.required],
+            })
+          )
+        );
+      });
   }
-  ngOnInit(): void {
- 
-  }
+  ngOnInit(): void {}
 
   submitAnswers(): void {
     const answers = this.questionForm.value;
     console.log('Submitted answers:', answers);
-    this.finalScore = this.getScore(answers) ; 
+    this.finalScore = this.getScore(answers);
     // Handle the submission logic here
   }
 
-  getScore(answers:any){
-    console.log(answers) ; 
-    let score : number = 0 ;  
-    for(let i = 0; i < this.arrQuestions.length ; i++){
-      let ans = answers[i].answer ; 
-      if(this.arrQuestions[i].type === 'true-false'){
-        if(this.arrQuestions[i].correctAnswer === ans.toString()){
-          score += 1 ; 
+  getScore(answers: any) {
+    console.log(answers);
+    let score: number = 0;
+    for (let i = 0; i < this.arrQuestions.length; i++) {
+      let ans = answers[i].answer;
+      if (this.arrQuestions[i].type === 'true-false') {
+        if (this.arrQuestions[i].correctAnswer === ans.toString()) {
+          score += 1;
         }
-      }
-      else{
-        if(this.arrQuestions[i].correctAnswer === ans){
-          score += 1 ; 
+      } else {
+        if (this.arrQuestions[i].correctAnswer === ans) {
+          score += 1;
         }
       }
     }
-    return score ; 
+    return score;
   }
   getFormGroup(index: number): FormGroup {
     return this.questionForm.at(index) as FormGroup;
   }
 
-  startAssessment(){
+  startAssessment() {
     this.hasStarted = true;
-    this.startTime = new Date(); 
+    this.startTime = new Date();
+    this.attendanceService.getAttendance().subscribe((data) => {
+      let totalAttendance = data.length;
+      console.log(totalAttendance) ; 
+      let newAttendance = new Attendance(totalAttendance + 1, parseInt(this.loggedUserId) , new Date() , "success");
+      this.attendanceService.addAttendance(newAttendance).subscribe(data => {
+        console.log("attendance added")
+      })
+    });
   }
 }
---
+++ src/app/services/attendance.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { Attendance } from '../models/attendance';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttendanceService {
+  baseUrl: string = 'http://localhost:3000';
+  httpHeader = {
+    headers: new HttpHeaders({
+      'content-type': 'application/json',
+    }),
+  };
+  constructor(private httpClient: HttpClient) { }
+  addAttendance(a:Attendance){
+    return this.httpClient.post<Attendance>(this.baseUrl + "/attendance" , a , this.httpHeader).pipe(catchError(this.httpError));
+  }
+  getAttendance(){
+    return this.httpClient.get<Attendance[]>(this.baseUrl + "/attendance" , this.httpHeader).pipe(catchError(this.httpError))
+  }
+  private httpError(error: HttpErrorResponse) {
+    let msg = '';
+    if (error.error instanceof ErrorEvent) {
+      msg = error.error.message;
+    } else {
+      msg = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(msg);
+    return throwError(msg);
+  }
+}
